d636add9c5a21f1222b7b609ff946e05cd8b2cb8
伍洪江git <wuhongjiang11@163.com>
2019-07-29 21:17:37 +0800

    first commit

---
+++ pages/web.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="box">
+  <div>web1</div>
+  <div>web2</div>
+  <div>web3</div>
+  <p>这个是文本1</p>
+  <p>这个是文本1</p>
+  <p>这个是文本1</p>
+
+  <p>这个是文本2</p>
+  <p>这个是文本2</p>
+  <p>这个是文本2</p>
+  
+  <p>这个是文本3</p>
+  <p>这个是文本3</p>
+  <p>这个是文本3</p>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
